5273b8c8cb7dc2c549b1a785e8dff3c7200968f1
geoni-ara <lgeeckh@gmail.com>
2026-08-04 10:40:14 +0900

    Feat: Item Definition(Data Asset) 추가

--- Source/CreatePotion/CreatePotion.Build.cs
+++ Source/CreatePotion/CreatePotion.Build.cs
@@ -18,7 +18,8 @@ public class CreatePotion : ModuleRules
 			"StateTreeModule",
 			"GameplayStateTreeModule",
 			"UMG",
-			"Slate"
+			"Slate",
+			"GameplayTags",
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
---
+++ Source/CreatePotion/Public/Data/CPForageableItemData.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "CPForageableItemData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAlchemyProperty
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag Tag;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 Value = 0;
+};
+
+UCLASS()
+class CREATEPOTION_API UCPForageableItemData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Identity")
+	FText DisplayName;
+	
+	// Tag
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Tag")
+	TArray<FAlchemyProperty> TagAxes;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Tag")
+	FAlchemyProperty SpecialProperty;
+	
+	// 인벤토리
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory", meta = (ClampMin = "1"))
+	int32 MaxStack;	
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory")
+	TSoftObjectPtr<UTexture2D> Icon;
+	
+	// Prop
+	// AActor -> 관련 액터 구현 시 변경
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Prop")
+	TSoftClassPtr<AActor> GatheringPropClass;
+	
+	// AActor -> 관련 액터 구현 시 변경
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Prop")
+	TSoftClassPtr<AActor> AlchemyPropClass;
+	
+	// 도감
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Codex")
+	TArray<FName> CodexTextKeys;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Codex")
+	TSoftObjectPtr<UTexture2D> CodexImage;
+};
