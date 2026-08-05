62ef6d000234570df3f87075087d0fa65d44b058
any0509 <annadevelop01@gmail.com>
2026-08-05 19:37:33 +0900

    ✨ update : CPInteractionPromptWidget 바인딩/언바인딩 및 텍스트 업데이트 함수 추가
    -🎉 add: WBP_HUD_Interaction 상호작용 UI 블루프린트 생성

--- Source/CreatePotion/Private/UI/Widgets/HUD/CPInteractionPromptWidget.cpp
+++ Source/CreatePotion/Private/UI/Widgets/HUD/CPInteractionPromptWidget.cpp
@@ -2,4 +2,26 @@
 
 
 #include "UI/Widgets/HUD/CPInteractionPromptWidget.h"
+#include "Components/TextBlock.h"
 
+void UCPInteractionPromptWidget::BindEvents()
+{
+	// TODO: 상호작용 탐색 컴포넌트 완성 후 델리게이트 구독
+	
+	SetVisibility(ESlateVisibility::Collapsed);
+}
+
+void UCPInteractionPromptWidget::UnbindEvents()
+{
+	// 구독한 델리게이트 해제 
+}
+
+void UCPInteractionPromptWidget::UpdateInteractionPrompt(bool bHasTarget, const FText& PromptText)
+{
+	SetVisibility(bHasTarget ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+	
+	if  (bHasTarget && TextBlock_InteractionPrompt)
+	{
+		TextBlock_InteractionPrompt->SetText(PromptText);
+	}
+}
--- Source/CreatePotion/Public/UI/Widgets/HUD/CPInteractionPromptWidget.h
+++ Source/CreatePotion/Public/UI/Widgets/HUD/CPInteractionPromptWidget.h
@@ -6,6 +6,7 @@
 #include "UI/Widgets/Base/CPBaseHUDWidget.h"
 #include "CPInteractionPromptWidget.generated.h"
 
+class UTextBlock;
 /**
  * 
  */
@@ -14,4 +15,17 @@ class CREATEPOTION_API UCPInteractionPromptWidget : public UCPBaseHUDWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	virtual void BindEvents() override;
+	virtual void UnbindEvents() override;
+	
+public:
+	// 상호작용 대상 탐색 컴포넌트가 호출할 함수
+	// bHasTarget: 지금 조준 중인 대상이 있는지
+	// PropmtText: "F: 채집하기" 같은 안내 문구
+	void UpdateInteractionPrompt(bool bHasTarget, const FText& PromptText);
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TextBlock_InteractionPrompt;
 };
