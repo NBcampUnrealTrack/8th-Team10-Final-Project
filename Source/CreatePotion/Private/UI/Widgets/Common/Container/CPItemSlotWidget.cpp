ddab2235e39cf46281ba10e08c4ecb90e2f2530b
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-06 19:56:19 +0900

    add : CPItemSlotWidget.h/cpp
    - 가장 기본이 되는 아이템 단일 슬롯 위젯

---
+++ Source/CreatePotion/Private/UI/Widgets/Common/Container/CPItemSlotWidget.cpp
@@ -0,0 +1,4 @@
+﻿// CPItemSlotWidget.h
+
+#include "UI/Widgets/Common/Container/CPItemSlotWidget.h"
+
---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Container/CPItemSlotWidget.h
@@ -0,0 +1,14 @@
+﻿// CPItemSlotWidget.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Base/CPBaseUserWidget.h"
+#include "CPItemSlotWidget.generated.h"
+
+UCLASS()
+class CREATEPOTION_API UCPItemSlotWidget : public UCPBaseUserWidget
+{
+	GENERATED_BODY()
+	
+};
