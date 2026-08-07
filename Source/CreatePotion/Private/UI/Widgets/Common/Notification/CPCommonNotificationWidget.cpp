b81094796a223fd35967ba3b61fa92b51cda10cd
any0509 <annadevelop01@gmail.com>
2026-08-07 17:40:25 +0900

    🎉 add: 공통 알림 위젯 생성
    ✨ update : 공통 알림 위젯 텍스트 설정/타이머 기능 추가

---
+++ Source/CreatePotion/Private/UI/Widgets/Common/Notification/CPCommonNotificationWidget.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widgets/Common/Notification/CPCommonNotificationWidget.h"
+#include "Components/TextBlock.h"
+
+void UCPCommonNotificationWidget::SetNotifiactionText(const FText& InText)
+{
+	if (TextBlock_Notification)
+	{
+		TextBlock_Notification->SetText(InText);
+	}
+}
+
+void UCPCommonNotificationWidget::ShowWithAutoClose(const FText& InText, float Duration)
+{
+	SetNotifiactionText(InText);
+	
+	if (Duration > 0.f)
+	{
+		GetWorld()->GetTimerManager().SetTimer(
+			AutoCloseTimerHandle,
+			this,
+			&UCPCommonNotificationWidget::RequestClose,
+			Duration,
+			false
+		);
+	}
+}
