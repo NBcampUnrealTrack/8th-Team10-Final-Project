d1f43d5d02cbbff6b3a739c842d2af785fc42c56
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-10 21:27:27 +0900

    feat : CPItemSlotWidget.h/cpp
    - 컨테이너 내 아이템 슬롯에 마우스 이벤트 로직 구현

--- Source/CreatePotion/Private/UI/Widgets/Common/Container/CPItemSlotWidget.cpp
+++ Source/CreatePotion/Private/UI/Widgets/Common/Container/CPItemSlotWidget.cpp
@@ -12,6 +12,19 @@ void UCPItemSlotWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	// 마우스 클릭 이벤트를 이 클래스가 처리할 수 있도록 Visible로 설정
+	SetVisibility(ESlateVisibility::Visible);
+
+	if (ItemIcon)
+	{
+		ItemIcon->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	}
+
+	if (StackText)
+	{
+		StackText->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+	}
+
 	ClearSlot();
 }
 
@@ -64,4 +77,84 @@ void UCPItemSlotWidget::ClearSlot()
 	{
 		StackText->SetVisibility(ESlateVisibility::Collapsed);
 	}
+}
+
+FReply UCPItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	// 좌클릭인 경우
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		// 빈 칸(데이터 없음)이면 무시하고 부모 함수로 처리
+		if (!CachedItemData.ItemDataAsset || !OwnerContainer)
+		{
+			return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+		}
+
+		if (InMouseEvent.IsLeftControlDown())
+		{
+			// TODO: [Ctrl + 좌클릭] 아이템 빠른 이동 (Quick Transfer)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 이동"), 
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();	// 이벤트를 처리했음을 엔진에 알림, 
+			// 마우스 클릭 이벤트가 마무리 되며 뒤 UI는 클릭 이벤트가 실행되지 않음
+		}
+		else if (InMouseEvent.IsShiftDown())
+		{
+			// TODO: [Shift + 좌클릭] 아이템 나누기 (Split)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 나누기"), 
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+		else if (InMouseEvent.IsAltDown())
+		{
+			// TODO: [Alt + 좌클릭] 아이템 버리기 (Drop)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 버리기"), 
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+		else
+		{
+			// TODO: [일반 좌클릭] '드래그 앤 드롭(Drag & Drop)'
+			return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+		}
+	}
+	else if (InMouseEvent.GetEffectingButton() == EKeys::RightMouseButton) // 우클릭인 경우
+	{
+		if (!CachedItemData.ItemDataAsset || !OwnerContainer)
+		{
+			return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+		}
+
+		if (InMouseEvent.IsLeftControlDown())
+		{
+			// TODO: [Ctrl + 우클릭] 아이템 사용 (Use Item)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 사용"),
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+		else if (InMouseEvent.IsShiftDown())
+		{
+			// TODO: [Shift + 우클릭] 아이템 세부 정보 표시 (Show Item Detail)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 세부 정보 표시"),
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+		else if (InMouseEvent.IsAltDown())
+		{
+			// TODO: [Alt + 우클릭] 장비 아이템 장착 (Equip Item)
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 장비 아이템 장착"),
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+		else
+		{
+			// TODO: [일반 우클릭] [LCtrl, Shift, Alt, 일반] + [좌, 우 클릭] 으로 가능한 6가지 UI 통합 UI 처리
+			UE_LOG(LogContainer, Warning, TEXT("[%s] 아이템 통합 UI"),
+				*CachedItemData.ItemDataAsset->DisplayName.ToString());
+			return FReply::Handled();
+		}
+	}
+
+	// 구현하고자 하는 기능 외의 경우, 마찬가지로 무시하고 부모 함수로 처리
+	return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
 }
