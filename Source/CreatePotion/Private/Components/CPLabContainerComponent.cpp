cc2d6852db624ec6361339bc0bb00d1dba165029
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-10 21:23:58 +0900

    add : CPLabContainerComponent.h/cpp
    - 공방의 가공 기구 하나에 부착할 컨테이너 컴포넌트
    - 슬롯 형식, 1슬롯에 1개의 재료 아이템만 선택 가능

---
+++ Source/CreatePotion/Private/Components/CPLabContainerComponent.cpp
@@ -0,0 +1,10 @@
+﻿// CPLabContainerComponent.h
+
+#include "Components/CPLabContainerComponent.h"
+
+UCPLabContainerComponent::UCPLabContainerComponent()
+{
+	ContainerType = EContainerType::Slot1D;
+	MaxSlots = 1;
+	MaxStack = 1;
+}
