2350cdf81b60d25f3c1262df259a16112e0cbfdf
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-12 16:28:50 +0900

    add : CPInventorySubsystem.h/cpp
    - 플레이어 인벤토리 데이터 유지를 위한 GameInstanceSubsystem

---
+++ Source/CreatePotion/Public/GameInstance/Subsystem/CPInventorySubsystem.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CPInventorySubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREATEPOTION_API UCPInventorySubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
