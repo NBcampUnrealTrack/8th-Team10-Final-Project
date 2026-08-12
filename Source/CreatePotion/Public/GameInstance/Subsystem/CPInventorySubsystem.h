7e38236084b029f2a36b6402e8a40e11715730d9
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-12 17:44:48 +0900

    feat : CPInventorySubsystem.h/cpp
    - 레벨 전환 시작 시 Inventory Subsystem에 현재 인벤토리 아이템 배열 저장
    - 레벨 전환 완료 시 Inventory Subsystem에 저장되어 있는 아이템 배열 로드

--- Source/CreatePotion/Public/GameInstance/Subsystem/CPInventorySubsystem.h
+++ Source/CreatePotion/Public/GameInstance/Subsystem/CPInventorySubsystem.h
@@ -1,4 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// CPInventorySubsystem.h
 
 #pragma once
 
@@ -6,12 +6,23 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "CPInventorySubsystem.generated.h"
 
-/**
- * 
- */
+struct FContainerItem;
+
 UCLASS()
 class CREATEPOTION_API UCPInventorySubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 	
+public:
+	void SaveInventoryData(const TArray<FContainerItem>& InItems);
+
+	bool LoadInventoryData(TArray<FContainerItem>& OutItems);
+
+	// 레벨 전환 시 임시로 보관할 인벤토리 배열
+	UPROPERTY()
+	TArray<FContainerItem> BackupInventoryItems;
+
+	// 백업된 데이터가 있는지 여부 (처음 게임 시작 시엔 안 부르기 위함)
+	UPROPERTY()
+	bool bHasBackup = false;
 };
