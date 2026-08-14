dbfcd01d31395ad5bf973109000062e7dc3c959a
any0509 <annadevelop01@gmail.com>
2026-08-14 20:26:27 +0900

    🎉 add: QuestSelectWidget 파일 생성

---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Quest/CPQuestSelectWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Base/CPBasePopupWidget.h"
+#include "CPQuestSelectWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREATEPOTION_API UCPQuestSelectWidget : public UCPBasePopupWidget
+{
+	GENERATED_BODY()
+};
