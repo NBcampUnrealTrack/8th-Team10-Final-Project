09c22d6625129ed2db0414551a65ebfa9ee429b2
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-18 19:31:34 +0900

    feat : CPPlayerController.h/cpp
    - 기존 UI 토글 및 컨테이너 설정을 모든 ContainerComponent에서 호환될 수 있도록 리팩토링

--- Source/CreatePotion/Private/Character/CPPlayerController.cpp
+++ Source/CreatePotion/Private/Character/CPPlayerController.cpp
@@ -6,18 +6,17 @@
 #include "InputMappingContext.h"
 #include "GameCore/Interface/CPLevelUIInterface.h"
 #include "GameFramework/HUD.h"
-#include "EngineUtils.h"		// Iterator
 
-#include "Lab/Actor/CPLabContainerActor.h"		// Lab에서 재료 선택 컨테이너 사용
+#include "CreatePotion.h"   // 로그용 헤더
 #include "UI/Widgets/Common/Container/CPContainerMainWidget.h"
 #include "UI/Widgets/Common/Container/CPContainerGridWidget.h"
 #include "Components/CPItemContainerComponent.h"
-#include "Components/CPLabContainerComponent.h"
+#include "UI/Context/CPContextInventoryOnly.h"
 
 void ACPPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SetContextHandlerForTargetContext(EUITargetContext::InventoryOnly);
 }
 
 void ACPPlayerController::SetupInputComponent()
@@ -39,49 +38,90 @@ void ACPPlayerController::SetupInputComponent()
 }
 
 #pragma region Container
-void ACPPlayerController::ToggleLabUI()
+void ACPPlayerController::SetContextHandlerForTargetContext(EUITargetContext InTargetContext)
 {
-	// 이미 열려있다면 닫기 (메모리에서 완전히 파괴)
-	if (LabUIInstance)
+	switch (InTargetContext)
 	{
-		LabUIInstance->RemoveFromParent();
-		LabUIInstance = nullptr; // 완벽한 파괴를 위해 참조를 끊는 nullptr 처리
-
-		CurrentInteractingContainer = nullptr;	// 현재 상호작용중인 컨테이너도 nullptr로 초기화
-
-		bShowMouseCursor = false;
-		SetInputMode(FInputModeGameOnly());
-		return;
-	}
-	
-	UCPLabContainerComponent* FoundLabComp = nullptr;
-	// TODO : 현재 Toggle시 매 번 레벨에서 모든 액터를 순회를 돌며 찾도록 되어있음
-	for (TActorIterator<ACPLabContainerActor> It(GetWorld()); It; ++It)
-	{
-		FoundLabComp = (*It)->LabContainerComponent;
+	case EUITargetContext::InventoryOnly:
+		CurrentContextHandler = NewObject<UCPContextInventoryOnly>(this);
+		break;
+	case EUITargetContext::Lab:
+		// TODO[Container] : UCPContextLab 구현 후 교체
+		CurrentContextHandler = NewObject<UCPContextInventoryOnly>(this);
+		break;
+	case EUITargetContext::Storage:
+		// TODO[Container] : UCPContextStorage 구현 후 교체
+		CurrentContextHandler = NewObject<UCPContextInventoryOnly>(this);
+		break;
+	case EUITargetContext::Shop:
+		// TODO[Container] : UCPContextShop 구현 후 교체
+		CurrentContextHandler = NewObject<UCPContextInventoryOnly>(this);
 		break;
 	}
+}
 
-	if (!FoundLabComp)
+void ACPPlayerController::OpenExternalContainerUI(UCPItemContainerComponent* InTargetContainer)
+{
+	if (!InTargetContainer || !InTargetContainer->ContainerUIClass)
+	{
+		UE_LOG(LogContainer, Warning, TEXT("[Container] TargetContainer가 없거나 ContainerUIClass가 설정되지 않았습니다."));
+		return;
+	}
+
+	// 이미 같은 컨테이너가 열려있다면 그대로 유지 (중복 생성 방지)
+	if (CurrentContainerUIInstance && CurrentInteractingContainer == InTargetContainer)
 	{
 		return;
 	}
-	
-	if (LabUIClass)
-	{
-		LabUIInstance = CreateWidget<UCPContainerMainWidget>(this, LabUIClass);
-		if (LabUIInstance)
-		{
-			LabUIInstance->AddToViewport();
-			
-			LabUIInstance->BindContainer(FoundLabComp);
-			CurrentInteractingContainer = FoundLabComp;
 
-			bShowMouseCursor = true;
-			SetInputMode(FInputModeGameAndUI());
-		}
+	// 다른 컨테이너가 열려있었다면 먼저 닫고 새로 엶
+	if (CurrentContainerUIInstance)
+	{
+		CloseExternalContainerUI();
 	}
+
+	CurrentContainerUIInstance = CreateWidget<UCPContainerMainWidget>(this, InTargetContainer->ContainerUIClass);
+	if (!CurrentContainerUIInstance)
+	{
+		return;
+	}
+
+	CurrentContainerUIInstance->AddToViewport();
+	CurrentContainerUIInstance->BindContainer(InTargetContainer);
+	CurrentInteractingContainer = InTargetContainer;
+
+	bShowMouseCursor = true;
+	SetInputMode(FInputModeGameAndUI());
+
+	SetContextHandlerForTargetContext(InTargetContainer->TargetContext);
+}
+
+void ACPPlayerController::CloseExternalContainerUI()
+{
+	if (CurrentContainerUIInstance)
+	{
+		CurrentContainerUIInstance->RemoveFromParent();
+		CurrentContainerUIInstance = nullptr;
+	}
+
+	CurrentInteractingContainer = nullptr;
+
+	bShowMouseCursor = false;
+	SetInputMode(FInputModeGameOnly());
+
+	// 컨테이너를 닫은 뒤에는 InventoryOnly로 변경
+	SetContextHandlerForTargetContext(EUITargetContext::InventoryOnly);
+}
+
+void ACPPlayerController::ToggleExternalContainerUI(UCPItemContainerComponent* InTargetContainer)
+{
+	// 같은 컨테이너가 이미 열려있다면 닫기
+	if (CurrentContainerUIInstance && CurrentInteractingContainer == InTargetContainer)
+	{
+		CloseExternalContainerUI();
+		return;
+	}
+
+	OpenExternalContainerUI(InTargetContainer);
 }
 #pragma endregion
-
-
--- Source/CreatePotion/Public/Character/CPPlayerController.h
+++ Source/CreatePotion/Public/Character/CPPlayerController.h
@@ -5,43 +5,64 @@
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
 #include "GameFramework/PlayerController.h"
+#include "Types/CPContainerTypes.h"
 #include "CPPlayerController.generated.h"
 
 class UInputAction;
 class UInputMappingContext;
 class UCPItemContainerComponent;
 class UCPContainerMainWidget;
+class UCPContainerContextBase;
 
 UCLASS()
 class CREATEPOTION_API ACPPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
-	UFUNCTION(BlueprintCallable, Category = "Test")
-	void ToggleLabUI();
-	
 protected:
 
 	/** Input Mapping Contexts */
-	UPROPERTY(EditAnywhere, Category ="Input|Input Mappings")
+	UPROPERTY(EditAnywhere, Category = "Input|Input Mappings")
 	TArray<UInputMappingContext*> DefaultMappingContexts;
-	
+
 	/** Gameplay initialization */
 	virtual void BeginPlay() override;
 
 	/** Input mapping context setup */
 	virtual void SetupInputComponent() override;
 
+#pragma region Container
 public:
+	// 대상 컨테이너의 ContainerUIClass를 읽어 UI를 열고 바인딩하는 범용 함수.
+	// Lab, Storage, Shop 등 어떤 외부형 컨테이너든 동일하게 처리 가능.
+	UFUNCTION(BlueprintCallable, Category = "Container|UI")
+	void OpenExternalContainerUI(UCPItemContainerComponent* InTargetContainer);
+
+	// 현재 열려있는 외부 컨테이너 UI를 닫는 범용 함수
+	UFUNCTION(BlueprintCallable, Category = "Container|UI")
+	void CloseExternalContainerUI();
+
+	// 상호작용(F키 등)에서 바로 호출하기 좋은 토글 버전.
+	// 이미 같은 컨테이너가 열려있으면 닫고, 아니면 엶.
+	UFUNCTION(BlueprintCallable, Category = "Container|UI")
+	void ToggleExternalContainerUI(UCPItemContainerComponent* InTargetContainer);
+
+private:
+	void SetContextHandlerForTargetContext(EUITargetContext InTargetContext);
+
+public:
+	// 현재 상호작용 중인 컨테이너
+	// TODO[Container] : F키 또는 컨테이너 이용 시 이 값을 해당 ItemContainerComponent로 설정해주어야 합니다.
 	UPROPERTY(BlueprintReadWrite, Category = "Container")
 	UCPItemContainerComponent* CurrentInteractingContainer = nullptr;
 
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
-	TSubclassOf<UCPContainerMainWidget> LabUIClass;
-
+	// Context를 처리할 
 	UPROPERTY()
-	UCPContainerMainWidget* LabUIInstance;
+	UCPContainerContextBase* CurrentContextHandler;
 
+protected:
+	// 현재 화면에 떠 있는 외부 컨테이너 UI 인스턴스 (Lab, Storage, Shop 공용)
+	UPROPERTY()
+	UCPContainerMainWidget* CurrentContainerUIInstance;
+#pragma endregion
 };
