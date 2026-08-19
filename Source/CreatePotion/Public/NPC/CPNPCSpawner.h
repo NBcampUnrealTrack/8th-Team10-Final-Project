fe140453f9d698414a5b2607cb003cd70f1f58f2
ShimSumin <sumannii036@gmail.com>
2026-08-19 16:50:07 +0900

    feat : NPC 스포너 퀘스트 ID 받아 관련 퀘스트 NPC 1명만 스폰하도록 변경

--- Source/CreatePotion/Public/NPC/CPNPCSpawner.h
+++ Source/CreatePotion/Public/NPC/CPNPCSpawner.h
@@ -2,29 +2,24 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Data/CPNPCDataAsset.h"
 #include "CPNPCSpawner.generated.h"
 
 class ACPBaseNPC;
-class UCPNPCDataAsset;
-class UQuestManager;
 
-// DA와 개별 스폰 트랜스폼을 액터에서 설정하기 위한 구조체
 USTRUCT(BlueprintType)
 struct FNPCSpawnConfig
 {
 	GENERATED_BODY()
 
-	// 스폰할 NPC의 Data Asset
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn")
 	TObjectPtr<UCPNPCDataAsset> NPCData = nullptr;
 
-	// 이 NPC에 개별 트랜스폼을 적용할지 여부
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn")
 	bool bUseCustomTransform = false;
 
-	// 개별 트랜스폼 (bUseCustomTransform가 true일 때만 디테일 패널에 활성화됨)
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC", meta = (EditCondition = "bUseCustomTransform"))
-	FTransform CustomTransform;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawn", meta = (EditCondition = "bUseCustomTransform"))
+	FTransform CustomTransform = FTransform::Identity;
 };
 
 UCLASS()
@@ -35,37 +30,17 @@ class CREATEPOTION_API ACPNPCSpawner : public AActor
 public:
 	ACPNPCSpawner();
 
+	// 선택한 퀘스트 ID에 해당하는 NPC 1명만 즉시 스폰
 	UFUNCTION(BlueprintCallable, Category = "NPC Spawner")
-	void StartSpawningSession();
-
-	// 현재 Accepted 상태인 Lab 퀘스트 ID들만 추려서 반환 (포션 리퀘스트 생성용)
-	TArray<FName> GetAcceptedLabQuestIDs(const UQuestManager* QuestManager) const;
-
-protected:
-	virtual void BeginPlay() override;
-
-private:
-	void SpawnNextNPC();
+	void SpawnNPC(FName QuestID);
 
 public:
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "NPC")
+	UPROPERTY(EditAnywhere, Category = "NPC")
 	TSubclassOf<ACPBaseNPC> NPCClass;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC|Spawn")
-	FTransform DefaultSpawnTransform;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC|Spawn")
+	UPROPERTY(EditAnywhere, Category = "NPC")
 	TArray<FNPCSpawnConfig> NPCSpawnConfigArray;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NPC|Spawn")
-	float SpawnInterval = 2.0f;
-
-private:
-	FTimerHandle SpawnTimerHandle;
-	int32 CurrentSpawnIndex = 0;
-
-	UPROPERTY()
-	TArray<FNPCSpawnConfig> FilteredNPCsToSpawn;
-
+	UPROPERTY(EditAnywhere, Category = "NPC")
+	FTransform DefaultSpawnTransform;
 };
