29c3672c3a700474ecc90be063d2faa02ee695df
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-19 21:25:22 +0900

    feat : CPItemSlotWidget.h/cpp
    - 마우스 좌클릭으로 아이템 집기/놓기를 토글하는 기능 구현을 위한 리팩토링
    (놓기의 경우 빈 칸에 처리를 해야하는데 기존의 빈 칸이라고 무조건 마우스 처리를 엔진 자체 처리하도록 넘기는 것 방지)
    - 마우스 좌클릭 한 번과 더블클릭의 경우를 처리할 수 있도록 기능 구현

--- Source/CreatePotion/Private/UI/Widgets/Common/Container/CPItemSlotWidget.cpp
+++ Source/CreatePotion/Private/UI/Widgets/Common/Container/CPItemSlotWidget.cpp
@@ -3,6 +3,7 @@
 #include "UI/Widgets/Common/Container/CPItemSlotWidget.h"
 #include "Components/Image.h"
 #include "Components/TextBlock.h"
+#include "GameFramework/InputSettings.h"			// 더블 클릭 입력 지연 시간을 사용하기 위해
 
 #include "CreatePotion.h"   // 로그용 헤더
 #include "Data/CPForageableItemData.h"
@@ -28,6 +29,16 @@ void UCPItemSlotWidget::NativeConstruct()
 	}
 
 	ClearSlot();
+}
+
+void UCPItemSlotWidget::NativeDestruct()
+{
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		World->GetTimerManager().ClearTimer(DoubleClickCheckHandler);
+	}
+	Super::NativeDestruct();
 }
 
 void UCPItemSlotWidget::UpdateSlot(const FContainerItem& ItemData)
@@ -83,14 +94,15 @@ void UCPItemSlotWidget::ClearSlot()
 
 FReply UCPItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
 {
-	// 빈 칸(유효한 데이터가 없음)이면 무시하고 부모 함수로 처리
-	// TODO[Container] : 아이템을 드롭할 때 빈 칸에 상호작용을 해야하기 때문에 수정해야할 부분
-	if (!CachedItemData.ItemDataAsset || !OwnerContainer)
+	ACPPlayerController* PC = Cast<ACPPlayerController>(GetOwningPlayer());
+	bool bIsHolding = PC && (PC->LeftClickPickedContainer != nullptr);
+
+	// 빈 칸이고 "현재 무언가를 들고 있지 않으면(bIsHolding값이 false일 때)" 기존대로 엔진 자체 처리
+	if ((!CachedItemData.ItemDataAsset && !bIsHolding) || !OwnerContainer)
 	{
 		return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
-	}
+	} // 빈 칸이라도 "현재 무언가를 들고 있으면(bIsHolding값이 true일 때)" 마우스 입력 처리를 위해 통과
 
-	ACPPlayerController* PC = Cast<ACPPlayerController>(GetOwningPlayer());
 	if (!PC || !PC->CurrentContextHandler)
 	{
 		return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
@@ -101,6 +113,7 @@ FReply UCPItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, c
 	{
 		return Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
 	}
+
 	bool bHandled = false;
 
 	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)	// 좌클릭인 경우
@@ -108,7 +121,18 @@ FReply UCPItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, c
 		if (InMouseEvent.IsLeftControlDown())      bHandled = ContextHandler->HandleCtrlLeftClick(this);
 		else if (InMouseEvent.IsShiftDown())       bHandled = ContextHandler->HandleShiftLeftClick(this);
 		else if (InMouseEvent.IsAltDown())         bHandled = ContextHandler->HandleAltLeftClick(this);
-		else                                       bHandled = ContextHandler->HandleLeftClickOnly(this);
+		else
+		{
+			// 일반 좌클릭을 즉시 실행하지 않고 타이머 대기 (더블 클릭 판별용)
+			float DoubleClickDelay = GetDefault<UInputSettings>()->DoubleClickTime;	// 엔진의 더블클릭 판정 시간
+			GetWorld()->GetTimerManager().SetTimer(
+				DoubleClickCheckHandler, 
+				this, 
+				&UCPItemSlotWidget::ExecuteLeftClickOnce, 
+				DoubleClickDelay,
+				false);
+			bHandled = true; // 일단 어떤 경우던 처리될 예정이니 Handled 처리
+		}
 	}
 	else if (InMouseEvent.GetEffectingButton() == EKeys::RightMouseButton)	// 우클릭인 경우
 	{
@@ -122,4 +146,42 @@ FReply UCPItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, c
 	// Context로 처리 했다면 FReply::Handled() 처리, 이벤트를 처리했음을 엔진에 알림
 	// 마우스 클릭 이벤트가 마무리 되며 뒤 UI는 클릭 이벤트가 실행되지 않음
 	return bHandled ? FReply::Handled() : Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+}
+
+void UCPItemSlotWidget::ExecuteLeftClickOnce()
+{
+	if (ACPPlayerController* PC = Cast<ACPPlayerController>(GetOwningPlayer()))
+	{
+		if (PC->CurrentContextHandler)
+		{
+			PC->CurrentContextHandler->HandleLeftClickOnly(this);
+		}
+	}
+}
+
+FReply UCPItemSlotWidget::NativeOnMouseButtonDoubleClick(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			// 더블 클릭이 들어왔으므로, 대기 중이던 일반(한 번) 좌클릭 타이머를 즉시 취소
+			World->GetTimerManager().ClearTimer(DoubleClickCheckHandler);
+		}
+
+		if (CachedItemData.ItemDataAsset && OwnerContainer)
+		{
+			if (ACPPlayerController* PC = Cast<ACPPlayerController>(GetOwningPlayer()))
+			{
+				if (PC->CurrentContextHandler)
+				{
+					bool bHandled = PC->CurrentContextHandler->HandleLeftDoubleClick(this);
+					return bHandled ? FReply::Handled() : Super::NativeOnMouseButtonDoubleClick(InGeometry, InMouseEvent);
+				}
+			}
+		}
+	}
+
+	return Super::NativeOnMouseButtonDoubleClick(InGeometry, InMouseEvent);
 }
--- Source/CreatePotion/Public/UI/Widgets/Common/Container/CPItemSlotWidget.h
+++ Source/CreatePotion/Public/UI/Widgets/Common/Container/CPItemSlotWidget.h
@@ -28,9 +28,16 @@ public:
 protected:
 	virtual void NativeConstruct() override;
 
-	// 마우스를 클릭했을 때
+	virtual void NativeDestruct() override;
+
+	// 마우스를 (한 번만) 클릭했을 때 발생하는 엔진 자체 이벤트 처리 함수
 	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 
+	// 마우스를 한 번만 클릭했을 때 실제로 실행될 함수
+	void ExecuteLeftClickOnce();
+
+	// 마우스를 더블클릭했을 때 발생하는 엔진 자체 이벤트 처리 함수
+	virtual FReply NativeOnMouseButtonDoubleClick(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 public:
 	// 현재 이 슬롯을 소유중인 컨테이너
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Container|Data")
@@ -47,4 +54,7 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 	UTextBlock* StackText;
 	
+private:
+	// 임계 시간동안 더블 클릭을 체크할 타이머
+	FTimerHandle DoubleClickCheckHandler;
 };
