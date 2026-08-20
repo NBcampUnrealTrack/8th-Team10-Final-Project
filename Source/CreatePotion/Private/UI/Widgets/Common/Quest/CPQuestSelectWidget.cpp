5ac609dd2652ecbd8b4f9fb4fa6f28cba271c2b8
any0509 <annadevelop01@gmail.com>
2026-08-20 11:24:16 +0900

    ✨ update  : QuestSelectWidget NPC 소환 로직 연결 완료

--- Source/CreatePotion/Private/UI/Widgets/Common/Quest/CPQuestSelectWidget.cpp
+++ Source/CreatePotion/Private/UI/Widgets/Common/Quest/CPQuestSelectWidget.cpp
@@ -13,7 +13,6 @@ void UCPQuestSelectWidget::BindEvents()
 {
 	Super::BindEvents();
 	
-	UE_LOG(LogTemp, Warning, TEXT("[QuestSelectWidget] 퀘스트 선택 창 BindEvents 실행됨"));
 	Button_CallCustomer->OnClicked.AddDynamic(this, &UCPQuestSelectWidget::HandleCallButtonClicked);
 	Button_Cancel->OnClicked.AddDynamic(this, &UCPQuestSelectWidget::HandleCancelButtonClicked);
 }
@@ -27,16 +26,23 @@ void UCPQuestSelectWidget::UnbindEvents()
 
 void UCPQuestSelectWidget::HandleCallButtonClicked()
 {
+	UE_LOG(LogTemp, Warning, TEXT("[QuestSelectWidget] HandleCallButtonClicked 함수 진입"));
 	// 임시 QuestId 선택 이후 Player가 조작 가능한 방법으로 변경
-	if (!FirstAcceptedQuestId()) return;
-	
-	if (SelectedQuestId.IsNone()) return;
+	if (SelectedQuestId.IsNone())
+	{
+		UE_LOG(LogTemp, Error, TEXT("[QuestSelectWidget] SelectedQuestId가 비어있습니다"));
+		return;
+	}
 	
 	UWorld* World = GetWorld();
 	if (!World) return;
 	
 	ACPNPCSpawner* Spawner = Cast<ACPNPCSpawner>(UGameplayStatics::GetActorOfClass(World, ACPNPCSpawner::StaticClass()));
-	if (!Spawner || !Spawner->SpawnNPC((SelectedQuestId))) return;
+	if (!Spawner || !Spawner->SpawnNPC((SelectedQuestId)))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[QuestSelectWidget] NPC 스포너가 설치되어있지 않습니다."));
+		return;
+	}
 	
 	ACPLabGameMode* LabGameMode = World->GetAuthGameMode<ACPLabGameMode>();
 	if (!LabGameMode || !LabGameMode->StartPotionRequest(SelectedQuestId)) return;
