70af3ac40e7de3ca1205d82d31182a5c5ae25c52
ShimSumin <sumannii036@gmail.com>
2026-08-21 19:13:39 +0900

    delete : 태그 선택 관련 코드 및 UI 삭제

--- Source/CreatePotion/Private/NPC/CPNPCSpawner.cpp
+++ Source/CreatePotion/Private/NPC/CPNPCSpawner.cpp
@@ -11,20 +11,6 @@ ACPNPCSpawner::ACPNPCSpawner()
 	PrimaryActorTick.bCanEverTick = false;
 }
 
-// 퀘스트 ID 입력받아 그 퀘스트의 NPC 스폰
-// 퀘스트 선택 UI에서 '손님 호출' 버튼을 눌렀을 때
-// 
-// [적용 예시 코드]
-// FName TargetQuestID = GetSelectedQuestID(); // 현재 UI에서 선택된 퀘스트 ID 가져오기
-// if (!TargetQuestID.IsNone())
-// {
-//     // 맵에 있는 스포너를 찾아 선택한 퀘스트의 NPC를 즉시 소환
-//     if (ACPNPCSpawner* Spawner = Cast<ACPNPCSpawner>(UGameplayStatics::GetActorOfClass(GetWorld(), ACPNPCSpawner::StaticClass())))
-//     {
-//         Spawner->SpawnNPC(TargetQuestID);
-//     }
-// }
-
 bool ACPNPCSpawner::SpawnNPC(FName QuestID)
 {
 	if (!NPCClass || QuestID.IsNone()) return false;
@@ -50,16 +36,6 @@ bool ACPNPCSpawner::SpawnNPC(FName QuestID)
 
 	if (!FoundConfig || !FoundConfig->NPCData) return false;
 
-	{
-		TArray<FCPLabPotionRequest> PotionRequests;
-		FCPLabPotionRequest Request;
-		Request.RequestId = QuestID;
-
-		Request.DisplayText = QuestManager->GetCurrentSessionHintText(QuestID);
-		PotionRequests.Add(Request);
-
-	}
-
 	const FTransform FinalTransform = FoundConfig->bUseCustomTransform ? FoundConfig->CustomTransform : DefaultSpawnTransform;
 
 	ACPBaseNPC* SpawnedNPC = GetWorld()->SpawnActorDeferred<ACPBaseNPC>(NPCClass, FinalTransform);
@@ -69,6 +45,13 @@ bool ACPNPCSpawner::SpawnNPC(FName QuestID)
 		UGameplayStatics::FinishSpawningActor(SpawnedNPC, FinalTransform);
 	}
 
-	// TODO : Lab게임 모드 쪽에 PotionRequests 전달
+	if (UWorld* World = GetWorld())
+	{
+		if (ACPLabGameMode* LabGameMode = World->GetAuthGameMode<ACPLabGameMode>())
+		{
+			LabGameMode->StartPotionRequest(QuestID);
+		}
+	}
+	
 	return true;
 }
