921e97f58bfbf1c3481238dc866341627182b1ee
geoni-ara <lgeeckh@gmail.com>
2026-08-21 20:08:23 +0900

    Feat : 태그 정의 DT 추가 및 채집물 도감 태그 표시 연동

---
+++ Source/CreatePotion/Public/Data/CPTagDefinitionTypes.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "CPTagDefinitionTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCPTagCombinationEntry
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tag")
+	FGameplayTag OtherTag;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tag")
+	FGameplayTag ResultTag;
+};
+
+USTRUCT(BlueprintType)
+struct FCPTagDefinitionRow : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tag")
+	FGameplayTag Tag;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tag")
+	FText DisplayName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tag")
+	TArray<FCPTagCombinationEntry> Combinations;
+};
