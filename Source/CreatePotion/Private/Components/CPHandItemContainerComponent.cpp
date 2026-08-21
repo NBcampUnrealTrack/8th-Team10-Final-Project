0157ab0f1a0a73aef408be88017cd4348c4e4949
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-21 20:53:01 +0900

    add : CPHandItemContainerComponent.cpp
    - "현재 들고 있는 아이템"을 임시로 보관할 Container Component

---
+++ Source/CreatePotion/Private/Components/CPHandItemContainerComponent.cpp
@@ -0,0 +1,10 @@
+﻿// CPHandItemContainerComponent.cpp
+
+#include "Components/CPHandItemContainerComponent.h"
+
+UCPHandItemContainerComponent::UCPHandItemContainerComponent()
+{
+	ContainerType = EContainerType::Slot1D;
+	MaxSlots = 1;
+	TargetContext = EUITargetContext::Hand;
+}
