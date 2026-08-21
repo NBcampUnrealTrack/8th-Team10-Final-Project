e2917d8450577bb269791246f6330a910b617c66
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-21 12:01:14 +0900

    add : CPBlankGridSlotWidget.h/cpp
    - 라우팅 정보를 처리할 UCPGridSlotWidgetBase를 상속
    - 라우팅 정보를 가지며 새롭게 빈 칸을 담당할 위젯

---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Container/CPBlankGridSlotWidget.h
@@ -0,0 +1,14 @@
+﻿// CPBlankGridSlotWidget.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Common/Container/CPGridSlotWidgetBase.h"
+#include "CPBlankGridSlotWidget.generated.h"
+
+UCLASS()
+class CREATEPOTION_API UCPBlankGridSlotWidget : public UCPGridSlotWidgetBase
+{
+	GENERATED_BODY()
+	
+};
