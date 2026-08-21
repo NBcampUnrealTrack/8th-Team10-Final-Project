02191a7a32ca43378a95537bc75d4b2462e0d538
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-21 16:41:32 +0900

    refactor : CPGridSlotWidgetBase.h/cpp
    - 기존 CPItemSlotWidget에서 처리하던 '한 번 클릭'을 CPItemSlotWidget의 새로운 부모 클래스인 CPGridSlotWidgetBase에서 처리하도록 리팩토링

--- Source/CreatePotion/Public/UI/Widgets/Common/Container/CPGridSlotWidgetBase.h
+++ Source/CreatePotion/Public/UI/Widgets/Common/Container/CPGridSlotWidgetBase.h
@@ -12,16 +12,29 @@ UCLASS()
 class CREATEPOTION_API UCPGridSlotWidgetBase : public UCPBaseUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void NativeDestruct() override;
+
+	// 좌클릭 한 번을 통한 라우팅 기능(아이템 집기/놓기/Swap 등)은 여기서 처리
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
 	
+	// 자식 클래스에서도 private 타이머를 취소시키기 위한 함수
+	void CancelLeftClickCheckHandler();
+
+private:
+	// 실제 좌클
+	void ExecuteLeftClickOnce();
+
 public:
 	// 모든 슬롯(빈 슬롯 포함)이 반드시 가져야 하는 정보
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Container|Data")
 	UCPItemContainerComponent* OwnerContainer;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Container|Data")
-	int32 GridIndex = -1;
+	int32 SlotGridIndex = -1;
 
-protected:
-	// 클릭을 통한 라우팅(아이템 집기/놓기/Swap 등)은 여기서 처리
-	// virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+private:
+	// 임계 시간동안 더블 클릭을 체크할 타이머
+	FTimerHandle DoubleClickCheckHandler;
 };
