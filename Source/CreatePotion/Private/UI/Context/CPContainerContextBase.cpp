169625d01292597c8ab4292fda06a1016b4b24a1
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-21 20:58:13 +0900

    feat : CPContainerContextBase.cpp
    - "현재 들고 있는 아이템"을 임시로 보관하는 로직 추가

--- Source/CreatePotion/Private/UI/Context/CPContainerContextBase.cpp
+++ Source/CreatePotion/Private/UI/Context/CPContainerContextBase.cpp
@@ -28,21 +28,37 @@ bool UCPContainerContextBase::HandleLeftClickOnly(UCPGridSlotWidgetBase* Clicked
             return false; // 빈 슬롯이면 false를 리턴하며 아무 것도 하지 않음
         }
 
-        // 아이템 집기 처리
-        PC->LeftClickPickedContainer = ClickedSlot->OwnerContainer;
-        PC->LeftClickPickedSlotIndex = ClickedSlot->SlotGridIndex;
+        FContainerItem PoppedItem;
+        if (ClickedSlot->OwnerContainer->PopItemFromContainer(ClickedSlot->SlotGridIndex, PoppedItem))
+        {
+            UE_LOG(LogContainer, Warning, TEXT("[Pick] 집기 성공: %s, 손 컨테이너 개수: %d"),
+                *PoppedItem.ItemDataAsset->DisplayName.ToString(), PC->LeftClickPickedContainer->ContainerItems.Num());
+
+            PoppedItem.GridIndex = 0; // 손-컨테이너는 칸이 1개뿐
+            PC->LeftClickPickedContainer->ContainerItems.Add(PoppedItem);
+            PC->LeftClickPickedContainer->OnContainerUpdated.Broadcast();
+
+            // 취소 시 되돌아갈 기존 Container의 종류와 index 정보 저장
+            PC->LeftClickPickedItemOriginContainer = ClickedSlot->OwnerContainer;
+            PC->LeftClickPickedOriginSlotIndex = ClickedSlot->SlotGridIndex;
+        }
+
         // TODO[Container] : 커서에 아이콘을 붙이는 드래그 비주얼 구현
         return true;
     }
     else // 무언가 들고 있었으면
     {
+        bool bIsPlacedAll = ClickedSlot->OwnerContainer->TryPlaceHoldingItem(
+            PC->LeftClickPickedContainer, ClickedSlot->ClickedGridIndex);
         // 기존 아이템과 스왑 또는 배치하는 함수
-        PC->LeftClickPickedContainer->MoveOrSwapItem(PC->LeftClickPickedSlotIndex, ClickedSlot->OwnerContainer, ClickedSlot->SlotGridIndex);
 
-        // 다 내려놓았으니 PC의 상태를 초기화
-        PC->ResetHoldingItem();
+        // 다 내려놓았을 땐 PC의 "현재 들고 있는 정보"를 초기화
+        if (bIsPlacedAll) 
+        {
+            PC->ResetHoldingItem();
+        }
 
-        // TODO: 마우스 커서를 원래대로 복구하는 로직 (하드웨어 커서 복구 또는 커서 위젯 숨기기)
+        // TODO: 마우스 커서를 원래대로 복구하는 로직 위치 (하드웨어 커서 복구 또는 커서 위젯 숨기기)
         return true;
     }
 }
@@ -60,6 +76,6 @@ bool UCPContainerContextBase::IsUsingInventoryOnly(ACPPlayerController* PC) cons
 
 bool UCPContainerContextBase::IsSlotFromInventory(UCPGridSlotWidgetBase* ClickedSlot, ACPPlayerController* PC) const
 {
-	return ClickedSlot && ClickedSlot->OwnerContainer
-		&& PC && (ClickedSlot->OwnerContainer == PC->CachedInventoryComponent);
+    return ClickedSlot && ClickedSlot->OwnerContainer
+        && PC && (ClickedSlot->OwnerContainer == PC->CachedInventoryComponent);
 }
