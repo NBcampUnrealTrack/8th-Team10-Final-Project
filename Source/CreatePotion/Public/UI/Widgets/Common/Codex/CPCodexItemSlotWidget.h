3a94ea232214ebf5ac287281a1a26e497dc868f2
any0509 <annadevelop01@gmail.com>
2026-08-21 11:20:47 +0900

    🎉 add : 도감용 ItemSlot c++ 파일 생성

---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Codex/CPCodexItemSlotWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Base/CPBaseUserWidget.h"
+#include "CPCodexItemSlotWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CREATEPOTION_API UCPCodexItemSlotWidget : public UCPBaseUserWidget
+{
+	GENERATED_BODY()
+};
