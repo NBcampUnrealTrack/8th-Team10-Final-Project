94cd9dae097d17c99b6c7d46a045fa5b6e48f14f
any0509 <annadevelop01@gmail.com>
2026-08-25 01:10:05 +0900

    🎉 add : 전체 도감 위젯 C++ 파일 추가
    - 아이템 도감 페이지 여는 기능 추가

---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Codex/CPCodexMainWidget.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Base/CPBasePopupWidget.h"
+#include "CPCodexMainWidget.generated.h"
+
+class UCPForageableItemData;
+class UCPForageableCodexWidget;
+class UCPCodexSubsystem;
+/**
+ * 
+ */
+UCLASS()
+class CREATEPOTION_API UCPCodexMainWidget : public UCPBasePopupWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	void BindEvents() override;
+	void UnbindEvents() override;
+
+private:
+	UFUNCTION()
+	void OpenItemDetail(UCPForageableItemData* SelectedItemData);
+
+public:
+	// 아이템 위젯 클래스
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Codex")
+	TSubclassOf<UUserWidget> ForageableCodexWidgetClass;
+
+protected:
+	// 도감 서브시스템
+	UPROPERTY(BlueprintReadOnly, Category = "Codex")
+	TObjectPtr<UCPCodexSubsystem> CodexSubsystem;
+};
