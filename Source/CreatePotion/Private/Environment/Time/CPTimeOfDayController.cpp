62d802a2181798865d20cd3daf0cec98efd9e830
0307nsu <nsu0307@gmail.com>
2026-08-25 21:18:41 +0900

    feat: 시간 변화에 따른 태양의 움직임 추가

---
+++ Source/CreatePotion/Private/Environment/Time/CPTimeOfDayController.cpp
@@ -0,0 +1,74 @@
+#include "Environment/Time/CPTimeOfDayController.h"
+#include "Engine/DirectionalLight.h"
+#include "GameInstance/Subsystem/CPTimeSubsystem.h"
+
+ACPTimeOfDayController::ACPTimeOfDayController()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ACPTimeOfDayController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UGameInstance* GI = GetGameInstance();
+	if (!GI) return;
+	
+	TimeSubsystem = GI->GetSubsystem<UCPTimeSubsystem>();
+	if (!TimeSubsystem) return;
+	
+	TargetGameMinutes = TimeSubsystem->GetTotalWorldMinutes();
+	VisualGameMinutes = TargetGameMinutes;
+	
+	UpdateSunRotation(VisualGameMinutes);
+	
+	TimeSubsystem->OnTimeChanged.AddDynamic(this, &ACPTimeOfDayController::HandleTimeChanged);
+	
+	GetWorldTimerManager().SetTimer(
+		VisualUpdateTimerHandle,
+		this,
+		&ACPTimeOfDayController::UpdateVisualTime,
+		0.1f,
+		true);
+}
+
+void ACPTimeOfDayController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (TimeSubsystem)
+	{
+		TimeSubsystem->OnTimeChanged.RemoveAll(this);
+	}
+	
+	Super::EndPlay(EndPlayReason);
+}
+
+void ACPTimeOfDayController::HandleTimeChanged(int64 NewMinutes)
+{
+	TargetGameMinutes = NewMinutes;
+}
+
+void ACPTimeOfDayController::UpdateSunRotation(double GameMinutes)
+{
+	if (!SunLight) return;
+	
+	constexpr double MinutesPerDay = 1440.0;
+	
+	const double MinutesOfDay = FMath::Fmod(GameMinutes, MinutesPerDay);
+	const double DayAlpha = MinutesOfDay / MinutesPerDay;
+	const float SunPitch = SunPitchOffset + static_cast<float>(DayAlpha * 360.0);
+	
+	SunLight->SetActorRotation(FRotator(SunPitch, SunYaw, 0.f));
+}
+
+void ACPTimeOfDayController::UpdateVisualTime()
+{
+	VisualGameMinutes = FMath::FInterpConstantTo(
+		VisualGameMinutes,
+		TargetGameMinutes,
+		0.1f,
+		1.f);
+	
+	UpdateSunRotation(VisualGameMinutes);
+}
+
+
---
+++ Source/CreatePotion/Public/Environment/Time/CPTimeOfDayController.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CPTimeOfDayController.generated.h"
+
+class ADirectionalLight;
+class UCPTimeSubsystem;
+
+// 시간에 따른 환경 변화 연출용 액터
+UCLASS()
+class CREATEPOTION_API ACPTimeOfDayController : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACPTimeOfDayController();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+private:
+	UFUNCTION()
+	void HandleTimeChanged(int64 NewMinutes);
+	
+	void UpdateSunRotation(double GameMinutes);
+	
+	void UpdateVisualTime();
+	
+private:
+	// Directional Light
+	UPROPERTY(EditAnywhere, Category = "Time")
+	TObjectPtr<ADirectionalLight> SunLight;
+	
+	// 태양 궤도 시작 각도
+	UPROPERTY(EditDefaultsOnly, Category = "Time")
+	float SunPitchOffset = -90.f;
+	
+	// 방향 조절용
+	UPROPERTY(EditDefaultsOnly, Category = "Time")
+	float SunYaw = 0.f;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UCPTimeSubsystem> TimeSubsystem;
+	
+	// 화면에 표시된 현재 시간
+	double VisualGameMinutes = 0.0;
+	
+	// 실제 게임 시간(목표 시간)
+	double TargetGameMinutes = 0.0;
+	
+	FTimerHandle VisualUpdateTimerHandle;
+};
