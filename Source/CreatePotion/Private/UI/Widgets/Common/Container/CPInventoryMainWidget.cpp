b75231d4366254922c884fba880d070585b432cc
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-27 21:05:46 +0900

    add : CPInventoryMainWidget.h/cpp
    - HandleCloseRequested() 함수를 오버라이드 하여 닫기 버튼이 눌렸을 때 인벤토리 UI를 Collapsed 처리

---
+++ Source/CreatePotion/Private/UI/Widgets/Common/Container/CPInventoryMainWidget.cpp
@@ -0,0 +1,16 @@
+﻿// CPInventoryMainWidget.cpp
+
+#include "UI/Widgets/Common/Container/CPInventoryMainWidget.h"
+#include "Character/CPPlayerController.h"
+#include "Components/CPInventoryComponent.h"
+
+void UCPInventoryMainWidget::HandleCloseRequested()
+{
+	if (ACPPlayerController* PC = Cast<ACPPlayerController>(GetOwningPlayer()))
+	{
+		if (PC->CachedInventoryComponent)
+		{
+			PC->CachedInventoryComponent->CloseInventoryUI();
+		}
+	}
+}
---
+++ Source/CreatePotion/Public/UI/Widgets/Common/Container/CPInventoryMainWidget.h
@@ -0,0 +1,17 @@
+﻿// CPInventoryMainWidget.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widgets/Common/Container/CPContainerMainWidget.h"
+#include "CPInventoryMainWidget.generated.h"
+
+UCLASS()
+class CREATEPOTION_API UCPInventoryMainWidget : public UCPContainerMainWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void HandleCloseRequested() override;
+
+};
