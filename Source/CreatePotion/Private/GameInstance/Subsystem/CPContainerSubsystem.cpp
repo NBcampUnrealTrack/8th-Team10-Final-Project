2ec348750146d8a5b00d099a23f3b8d5fb5d4d15
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-08-27 12:39:16 +0900

    add : CPContainerSubsystem.h/cpp
    - 기존 CPInventorySubsystem에서 Container 범용으로 사용하기 위한 리팩토링

---
+++ Source/CreatePotion/Private/GameInstance/Subsystem/CPContainerSubsystem.cpp
@@ -0,0 +1,25 @@
+﻿// CPContainerSubsystem.cpp
+
+#include "GameInstance/Subsystem/CPContainerSubsystem.h"
+
+#include "CreatePotion.h"				// 로그용 헤더
+#include "Types/CPContainerTypes.h"		// 아이템 구조체
+
+void UCPContainerSubsystem::SaveContainerData(const TArray<FContainerItem>& InItems)
+{
+	BackupContainerItems = InItems;
+	bHasBackup = true;
+	UE_LOG(LogContainer, Log, TEXT("[ContainerSubsystem] 컨테이너 데이터 %d개 백업 완료"),
+		BackupContainerItems.Num());
+}
+
+bool UCPContainerSubsystem::LoadContainerData(TArray<FContainerItem>& OutItems)
+{
+	if (bHasBackup)
+	{
+		OutItems = BackupContainerItems;
+		UE_LOG(LogContainer, Log, TEXT("[ContainerSubsystem] 컨테이너 데이터 복원 완료"));
+		return true;
+	}
+	return false;
+}
---
+++ Source/CreatePotion/Public/GameInstance/Subsystem/CPContainerSubsystem.h
@@ -0,0 +1,29 @@
+﻿// CPContainerSubsystem.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CPContainerSubsystem.generated.h"
+
+struct FContainerItem;
+
+UCLASS()
+class CREATEPOTION_API UCPContainerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	void SaveContainerData(const TArray<FContainerItem>& InItems);
+
+	bool LoadContainerData(TArray<FContainerItem>& OutItems);
+
+	// 레벨 전환 시 임시로 보관할 인벤토리 배열
+	UPROPERTY()
+	TArray<FContainerItem> BackupContainerItems;
+
+	// 백업된 데이터가 있는지 여부 (처음 게임 시작 시엔 안 부르기 위함)
+	UPROPERTY()
+	bool bHasBackup = false;
+};
+
