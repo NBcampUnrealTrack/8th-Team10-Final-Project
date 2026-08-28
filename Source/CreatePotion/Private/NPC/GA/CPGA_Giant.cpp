d355adbb0e095eed9133e9c9db4cb056566a32e3
ShimSumin <sumannii036@gmail.com>
2026-08-28 19:04:43 +0900

    delete : 필요없는 Test 에셋 및 코드 삭제

--- Source/CreatePotion/Private/NPC/GA/CPGA_Giant.cpp
+++ Source/CreatePotion/Private/NPC/GA/CPGA_Giant.cpp
@@ -15,16 +15,10 @@ void UCPGA_Giant::ApplyVisual(ACPBaseNPC* NPC, bool bActive, float InMagnitude)
 {
 	if (!NPC || !NPC->GetMesh()) return;
 
-	UE_LOG(LogTemp, Warning, TEXT("[GA Execution] 1. Giant ApplyVisual START (Frame: %llu, Current Mesh Scale: %s)"),
-		GFrameCounter,
-		*NPC->GetMesh()->GetRelativeScale3D().ToString());
-
 	const FVector BaseScale = NPC->GetBaseMeshScale();
 	const FVector TargetScale = bActive ? (BaseScale * InMagnitude) : BaseScale;
 
 	NPC->GetMesh()->SetRelativeScale3D(TargetScale);
 	NPC->FitCapsuleToMesh(NPC->GetMesh()->GetSkeletalMeshAsset());
 
-	UE_LOG(LogTemp, Warning, TEXT("[GA Execution] 1. Giant ApplyVisual END (New Mesh Scale: %s)"),
-		*NPC->GetMesh()->GetRelativeScale3D().ToString());
 }
