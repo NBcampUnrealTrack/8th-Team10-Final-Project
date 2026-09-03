7a60e70918c6c1634065ab5421e2c89a444bddac
Opalian <opalian94@gmail.com>
2026-09-03 15:17:27 +0900

    feat: GamePlay Cue 추가, Impulse 변경

--- Source/CreatePotion/Private/Character/GAS/Abilities/GA_CPPlayerFartLaunch.cpp
+++ Source/CreatePotion/Private/Character/GAS/Abilities/GA_CPPlayerFartLaunch.cpp
@@ -1,9 +1,7 @@
 #include "Character/GAS/Abilities/GA_CPPlayerFartLaunch.h"
 
 #include "AbilitySystemComponent.h"
-#include "Abilities/GameplayAbilityTypes.h"
 #include "GameFramework/Character.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "TimerManager.h"
 
 UGA_CPPlayerFartLaunch::UGA_CPPlayerFartLaunch()
@@ -17,13 +15,6 @@ UGA_CPPlayerFartLaunch::UGA_CPPlayerFartLaunch()
 	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName(TEXT("Immunity.Potion.All"))));
 	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName(TEXT("Immunity.Potion.FartLaunch"))));
 	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName(TEXT("State.Reaction.Potion"))));
-
-	FartLaunchCueTag = FGameplayTag::RequestGameplayTag(FName(TEXT("GameplayCue.Potion.FartLaunch")));
-
-	FAbilityTriggerData TriggerData;
-	TriggerData.TriggerTag = GetFartLaunchEventTag();
-	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
-	AbilityTriggers.Add(TriggerData);
 }
 
 void UGA_CPPlayerFartLaunch::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
@@ -31,18 +22,12 @@ void UGA_CPPlayerFartLaunch::ActivateAbility(const FGameplayAbilitySpecHandle Ha
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
 	ACharacter* PlayerCharacter = ActorInfo ? Cast<ACharacter>(ActorInfo->AvatarActor.Get()) : nullptr;
-	UCharacterMovementComponent* MovementComponent = IsValid(PlayerCharacter) ? PlayerCharacter->GetCharacterMovement() : nullptr;
 	UWorld* World = GetWorld();
-	const bool bHasExpectedEvent = TriggerEventData && TriggerEventData->EventTag.MatchesTagExact(GetFartLaunchEventTag());
-
-	if (!IsValid(PlayerCharacter) || !IsValid(MovementComponent) || !World || !PlayerCharacter->HasAuthority() || !bHasExpectedEvent)
+	if (!IsValid(PlayerCharacter) || !World || !HasAuthority(&ActivationInfo))
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
 	}
-
-	TargetCharacter = PlayerCharacter;
-	CacheOriginalMovementState(PlayerCharacter);
 
 	if (FartLaunchCueTag.IsValid() && ActorInfo->AbilitySystemComponent.IsValid())
 	{
@@ -51,73 +36,22 @@ void UGA_CPPlayerFartLaunch::ActivateAbility(const FGameplayAbilitySpecHandle Ha
 
 	PlayerCharacter->AddActorWorldOffset(FVector(0.0f, 0.0f, 50.0f), false);
 
-	MovementComponent->FallingLateralFriction = 0.0f;
-	MovementComponent->SetMovementMode(EMovementMode::MOVE_Falling);
-
-	ApplyCharacterImpulse();
+	const FVector LaunchDirection = PlayerCharacter->GetActorTransform().TransformVectorNoScale(LaunchRotation.Vector()).GetSafeNormal();
+	PlayerCharacter->LaunchCharacter(LaunchDirection * LaunchForce, true, true);
 
 	if (ReactionDuration <= 0.0f)
 	{
-		EndImpulseBehavior();
+		EndLaunchBehavior();
 	}
 	else
 	{
-		World->GetTimerManager().SetTimer(DurationTimerHandle, this, &UGA_CPPlayerFartLaunch::EndImpulseBehavior, ReactionDuration, false);
+		World->GetTimerManager().SetTimer(DurationTimerHandle, this, &UGA_CPPlayerFartLaunch::EndLaunchBehavior, ReactionDuration, false);
 	}
 }
 
-void UGA_CPPlayerFartLaunch::EndImpulseBehavior()
+void UGA_CPPlayerFartLaunch::EndLaunchBehavior()
 {
 	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
-}
-
-void UGA_CPPlayerFartLaunch::ApplyCharacterImpulse()
-{
-	ACharacter* PlayerCharacter = TargetCharacter.Get();
-	UCharacterMovementComponent* MovementComponent = IsValid(PlayerCharacter) ? PlayerCharacter->GetCharacterMovement() : nullptr;
-	if (!IsValid(MovementComponent))
-	{
-		EndImpulseBehavior();
-		return;
-	}
-
-	const FVector BaseImpulseDirection = PlayerCharacter->GetActorTransform().TransformVectorNoScale(LaunchRotation.Vector()).GetSafeNormal();
-	const FVector ScatteredImpulseDirection = FMath::VRandCone(BaseImpulseDirection, FMath::DegreesToRadians(5.0f));
-	MovementComponent->AddImpulse(ScatteredImpulseDirection * LaunchForce, true);
-}
-
-void UGA_CPPlayerFartLaunch::CacheOriginalMovementState(ACharacter* InTargetCharacter)
-{
-	bHasCachedOriginalMovementState = false;
-	UCharacterMovementComponent* MovementComponent = IsValid(InTargetCharacter) ? InTargetCharacter->GetCharacterMovement() : nullptr;
-	if (!IsValid(MovementComponent))
-	{
-		return;
-	}
-
-	OriginalMovementMode = MovementComponent->MovementMode;
-	OriginalCustomMovementMode = MovementComponent->CustomMovementMode;
-	OriginalFallingLateralFriction = MovementComponent->FallingLateralFriction;
-	bHasCachedOriginalMovementState = true;
-}
-
-void UGA_CPPlayerFartLaunch::RestoreOriginalMovementState(ACharacter* InTargetCharacter)
-{
-	if (!IsValid(InTargetCharacter) || !bHasCachedOriginalMovementState)
-	{
-		return;
-	}
-
-	if (UCharacterMovementComponent* MovementComponent = InTargetCharacter->GetCharacterMovement())
-	{
-		MovementComponent->FallingLateralFriction = OriginalFallingLateralFriction;
-
-		const bool bWasGroundMovement = OriginalMovementMode == MOVE_Walking || OriginalMovementMode == MOVE_NavWalking;
-		const EMovementMode RestoredMovementMode = bWasGroundMovement && !MovementComponent->IsMovingOnGround() ? MOVE_Falling : OriginalMovementMode;
-		MovementComponent->SetMovementMode(RestoredMovementMode, OriginalCustomMovementMode);
-	}
-
-	bHasCachedOriginalMovementState = false;
 }
 
 void UGA_CPPlayerFartLaunch::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
@@ -132,14 +66,5 @@ void UGA_CPPlayerFartLaunch::EndAbility(const FGameplayAbilitySpecHandle Handle,
 		ActorInfo->AbilitySystemComponent->RemoveGameplayCue(FartLaunchCueTag);
 	}
 
-	RestoreOriginalMovementState(TargetCharacter.Get());
-	TargetCharacter.Reset();
-
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
-}
-
-const FGameplayTag& UGA_CPPlayerFartLaunch::GetFartLaunchEventTag()
-{
-	static const FGameplayTag FartLaunchEventTag = FGameplayTag::RequestGameplayTag(FName(TEXT("Potion.Effect.FartLaunch")));
-	return FartLaunchEventTag;
 }
--- Source/CreatePotion/Public/Character/GAS/Abilities/GA_CPPlayerFartLaunch.h
+++ Source/CreatePotion/Public/Character/GAS/Abilities/GA_CPPlayerFartLaunch.h
@@ -2,14 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "GA_CPPlayerFartLaunch.generated.h"
 
-class ACharacter;
-
 /**
- * Potion.Effect.FartLaunch를 받은 플레이어 전용 Reaction Ability.
- * 플레이어를 래그돌로 전환하지 않고 일정 시간 전방 위쪽으로 추진한다.
+ * C++ contract for the player reaction to Potion.Effect.FartLaunch.
+ * Blueprint children configure the event trigger, cue, tuning, and presentation.
  */
 UCLASS(Blueprintable)
 class CREATEPOTION_API UGA_CPPlayerFartLaunch : public UGameplayAbility
@@ -24,34 +21,21 @@ public:
 
 private:
 	UFUNCTION()
-	void EndImpulseBehavior();
-
-	void ApplyCharacterImpulse();
-
-	void CacheOriginalMovementState(ACharacter* InTargetCharacter);
-	void RestoreOriginalMovementState(ACharacter* InTargetCharacter);
-	static const FGameplayTag& GetFartLaunchEventTag();
+	void EndLaunchBehavior();
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Potion|FartLaunch", meta = (ClampMin = "0.0", Units = "s"))
 	float ReactionDuration = 2.5f;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Potion|FartLaunch", meta = (ClampMin = "0.0"))
-	float LaunchForce = 1500.0f;
+	float LaunchForce = 1000.0f;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Potion|FartLaunch")
-	FRotator LaunchRotation = FRotator(45.0f, 0.0f, 0.0f);
+	FRotator LaunchRotation = FRotator(55.0f, 0.0f, 0.0f);
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Potion|FartLaunch")
 	FGameplayTag FartLaunchCueTag;
 
 private:
-	TWeakObjectPtr<ACharacter> TargetCharacter;
-
-	EMovementMode OriginalMovementMode = MOVE_Walking;
-	uint8 OriginalCustomMovementMode = 0;
-	float OriginalFallingLateralFriction = 0.0f;
-	bool bHasCachedOriginalMovementState = false;
-
 	FTimerHandle DurationTimerHandle;
 };
